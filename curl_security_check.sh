02ecc954d255a54ecca961d5c18df7ec2554ba33
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-17 13:07:34 -0400

    Update curl_security_check.sh

--- curl_security_check.sh
+++ curl_security_check.sh
@@ -1,7 +1,8 @@
 curl_security_check () {
     local tmpfile=$(mktemp)  # Create a temporal file in the default temporal folder of the system
     # Lets do some magic for the tmpfile to be removed when this script ends, even if it crashes
-    local exec {FD_W}>"$tmpfile"  # Create file descriptor for writing, using first number available
+    # local exec {FD_W}>"$tmpfile"  # Create file descriptor for writing, using first number available
+    local exec FD_W>"$tmpfile"  # Create file descriptor for writing, using first number available
     local exec {FD_cat}<"$tmpfile"  # Create file descriptor for reading, using first number available
     local exec {FD_grep}<"$tmpfile"  # Create file descriptor for reading, using first number available
     rm "$tmpfile"  # Delete the file, but file descriptors keep available for this script
