ee127bad07bf08058988a4679c2c7fd7fde2c110
Nardberjean <Nardberjean@users.noreply.github.com>
2018-05-17 13:16:10 -0400

    Update curl_security_check.sh

--- curl_security_check.sh
+++ curl_security_check.sh
@@ -3,7 +3,9 @@ curl_security_check () {
     # Lets do some magic for the tmpfile to be removed when this script ends, even if it crashes
     # local exec {FD_W}>"$tmpfile"  # Create file descriptor for writing, using first number available
     local exec FD_W>"$tmpfile"  # Create file descriptor for writing, using first number available
+    echo FD_W
     local exec {FD_cat}<"$tmpfile"  # Create file descriptor for reading, using first number available
+    echo FD_cat
     local exec {FD_grep}<"$tmpfile"  # Create file descriptor for reading, using first number available
     rm "$tmpfile"  # Delete the file, but file descriptors keep available for this script
 
