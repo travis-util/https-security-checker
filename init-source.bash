0a89a6c820ac84f51258f71c34ef637cf2f30e00
Nardberjean <Nardberjean@users.noreply.github.com>
2018-11-07 13:46:17 -0500

    Update init-source.bash

--- init-source.bash
+++ init-source.bash
@@ -1,4 +1,5 @@
 curl_security_check () {
+    # 'tee' command could also have been used, instead of file descriptors.
     local tmpfile=$(mktemp)  # Create a temporal file in the default temporal folder of the system
     # Lets do some magic for the tmpfile to be removed when this script ends, even if it crashes
     local exec {FD_W}>"$tmpfile"  # Create file descriptor for writing, using first number available
@@ -10,5 +11,5 @@ curl_security_check () {
     curl -H "Accept: text/plain" https://security.sensiolabs.org/check_lock -F lock=@$1 >&$FD_W
     # exec $FD_W>&- # Closes file descriptor cannot be done
     cat <&$FD_cat
-    grep "No known\* vulnerabilities detected." <&$FD_grep
+    grep "No packages have known vulnerabilities." <&$FD_grep
   }
